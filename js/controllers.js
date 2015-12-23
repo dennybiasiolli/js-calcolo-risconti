cfd1d3c4ed1a65c0381cff3de1ebf7d2e2b4a945
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2015-12-23 10:19:21 +0100

    Aggiornato calcolo includendo il giorno iniziale come competenza

--- js/controllers.js
+++ js/controllers.js
@@ -3,34 +3,34 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-    .controller('myCtrl', ['$scope', '$window', function($scope, $window) {
-        $scope.calcolaRisconti = function(){
+    .controller('myCtrl', ['$scope', '$window', function ($scope, $window) {
+        $scope.calcolaRisconti = function () {
             $scope.arrValori = [];
             $scope.ImportoTotCalcolato = 0;
             $scope.diffAnni = 0;
             $scope.diffGiorni = 0;
-            if(!$scope.Importo) $scope.Importo = 0;
+            if (!$scope.Importo) $scope.Importo = 0;
 
-            if($scope.DataInizio && $scope.DataFine){
+            if ($scope.DataInizio && $scope.DataFine) {
                 var annoInizio = $scope.DataInizio.getFullYear();
                 var annoFine = $scope.DataFine.getFullYear();
                 $scope.diffAnni = annoFine - annoInizio + 1;
                 var timeDiff = ($scope.DataFine.getTime() - $scope.DataInizio.getTime());
-                $scope.diffGiorni = Math.ceil(timeDiff / (1000 * 3600 * 24)) + 1;
-                if($scope.diffGiorni > 0){
+                $scope.diffGiorni = Math.ceil(timeDiff / (1000 * 3600 * 24)) + ($scope.IncludiDataInizio ? 1 : 0);
+                if ($scope.diffGiorni > 0) {
                     var i = 0;
-                    for(var annoAttuale = annoInizio; annoAttuale <= annoFine; annoAttuale++){
+                    for (var annoAttuale = annoInizio; annoAttuale <= annoFine; annoAttuale++) {
                         var myDataInizio = new Date(annoAttuale, 0, 1, 0, 0, 0);
                         //se sono nel primo anno del ciclo, considero la data di inizio ufficiale
-                        if(annoAttuale == annoInizio)
+                        if (annoAttuale == annoInizio)
                             myDataInizio = $scope.DataInizio;
 
                         var myDataFine = new Date(annoAttuale, 11, 31, 0, 0, 0);
                         //se sono nell'ultimo anno del ciclo, considero la data di fine ufficiale
-                        if(annoAttuale == annoFine)
+                        if (annoAttuale == annoFine)
                             myDataFine = $scope.DataFine;
 
-                        var addGiornoCalc = (annoAttuale == annoInizio ? 0 : 1);
+                        var addGiornoCalc = (annoAttuale == annoInizio && !$scope.IncludiDataInizio ? 0 : 1);
                         var myGiorni = Math.ceil((myDataFine.getTime() - myDataInizio.getTime()) / (1000 * 3600 * 24)) + addGiornoCalc;
                         var myImporto = ($scope.Importo * myGiorni) / $scope.diffGiorni;
                         $scope.ImportoTotCalcolato += myImporto;
@@ -53,6 +53,7 @@ angular.module('myApp.controllers', [])
         $scope.DataInizio = new Date(dataAttuale.getFullYear(), dataAttuale.getMonth(), dataAttuale.getDate());
         $scope.DataFine = new Date(dataAttuale.getFullYear() + 1, dataAttuale.getMonth(), dataAttuale.getDate());
         $scope.DataFine.setDate($scope.DataFine.getDate() - 1);
+        $scope.IncludiDataInizio = true;
         //$scope.DataInizio = new Date(2014, 6, 1);
         //$scope.DataFine = new Date(2015, 5, 30);
         //$scope.Importo = 123;
